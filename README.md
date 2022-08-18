a08e2a96d3c898b4f0ff2b6ac1a1ecc633ffd92c
Ryan Lewis <rlewis94@gmail.com>
2022-08-18 13:26:58 -0700

    switched to form submission template

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # java_resume_builder
 finding a way to make a resume PDF from json which is able to be parsed by tools like https://resume-parser.vercel.app/
 
-currently just a sample website created using: https://spring.io/quickstart
+currently just a sample website created using: https://spring.io/guides/gs/handling-form-submission/
+
+# PDF generation
+starting to follow this guide: https://www.baeldung.com/java-pdf-creation
+added the dependencies and nothing else yet
+
+# website integration
+Need to add a button on the site that triggers the pdf generation
--- src/main/java/com/ryanlewis/resume_builder/ResumeBuilderApplication.java
+++ src/main/java/com/ryanlewis/resume_builder/ResumeBuilderApplication.java
@@ -1,13 +1,13 @@
-package com.ryanlewis.resume_builder;
+// package com.ryanlewis.resume_builder;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ResumeBuilderApplication {
+// @SpringBootApplication
+// public class ResumeBuilderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResumeBuilderApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(ResumeBuilderApplication.class, args);
+// 	}
 
-}
+// }
